c8dad92484b6cc8b8add8ae1ba07c5436908c37a
Prasses Khadka <khadkaprasses@gmail.com>
2023-07-06 12:02:38 +0545

    Learning react useState and useEffect hooks

---
+++ reac-hooks/src/App.js
@@ -0,0 +1,44 @@
+import React,{useState,useEffect} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    const [count,setCount]=useState(0);
+    const incrementCount=()=>{
+    setCount(count+1);
+  };
+  const [color,setColor]=useState('black');
+  useEffect(()=>{
+    const changeColorOnClick=()=>{
+      if(color==='black'){
+        setColor('red');
+      }else{
+        setColor('black');
+      }
+    }
+
+    document.addEventListener('click',changeColorOnClick);
+    return ()=>{
+      document.removeEventListener('click',changeColorOnClick);
+    };
+  },[color])
+
+  return(
+    <div>
+      <div>{count}</div>
+      <button onClick={incrementCount}>Increment</button> 
+
+     <div id="myDiv" style={{
+       color: "white",
+       width: "100px",
+       height: "100px",
+       position: "absolute",
+       left: "50%",
+       top: "50%",
+       backgroundColor: color,
+      }}>This div can change color. Click on me!</div>
+      </div>
+  )
+}
+
+export default App;
